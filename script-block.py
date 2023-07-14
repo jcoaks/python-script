75e1c844a85136596b1dc1920f28d5c4a3ef9a0b
Juan Robles <robles.juanc@gmail.com>
2023-07-14 11:30:24 -0400

    update

--- script-block.py
+++ script-block.py
@@ -8,8 +8,11 @@ import requests
 import time
 
 number_threads = 64
-smart_contract_alias = 'CENSO__sc02'
+by = 'CENSO'
+scope = 'CENSO__SC'
+smart_contract_alias = 'CENSO__SC'
 node_url = 'https://prod-001-node.cloud.nxtfi.org/v2'
+#node_url = 'http://localhost:5300/development/v2'
 service_url = 'http://localhost:3050'
 scans_folders = ['sc01', 'sc02', 'sc03', 'sc04', 'sc05', 'sc06', 'sc07', 'sc08', 'sc09', 'sc10', 'sc11', 'sc12', 'sc13', 'sc14', 'sc15', 'sc16', 'sc17']
 #scans_folders = ['sc18', 'sc19', 'sc20', 'sc21', 'sc22', 'sc23', 'sc24', 'sc25', 'sc26', 'sc27', 'sc28', 'sc29', 'sc30', 'sc31', 'sc32', 'sc33', 'sc34']
@@ -30,8 +33,7 @@ class send_block_thread(threading.Thread):
         block = self.__block
         x3 = requests.post(node_url + '/newBlock', data = block)
         result = json.loads(x3.text)
-        print("Block sended: ", block)
-        print("Block result: ", result)
+        print("Block result: ", block)
 
 
 def get_images_json(file, filename, scanner, pallet, box):
@@ -53,7 +55,7 @@ def get_images_json(file, filename, scanner, pallet, box):
 
     return data
 
-def send_block(data_field, last_block):
+def send_block(data_field, last_block, counter):
     data_field = "// IMPORT " + smart_contract_alias + "\n\n" + data_field
     block = {
         'prevHash': last_block['head'],
@@ -61,8 +63,8 @@ def send_block(data_field, last_block):
         'version': 2,
         'data': data_field,
         'timestamp': round(time.time() * 1000),
-        'scope': 'CENSO__sc02',
-        'by': 'CENSO'
+        'scope': scope,
+        'by': by
     }
     sign_result = requests.post(service_url + '/sign-block', data = block)
     signature = json.loads(sign_result.text)
@@ -73,8 +75,9 @@ def send_block(data_field, last_block):
 
     block['by'] = block['by']
     block['hash'] = hash['hash']
-    print("Build block: ", block)
+    #print("Build block: ", block)
 
+    #print("Block sended ", counter)
     send_block_thread(json.dumps(block, indent=4)).start()
 
     return {
@@ -86,15 +89,26 @@ def search_files(path):
     head_result = requests.get(node_url + '/_head/' + smart_contract_alias)
     last_block = json.loads(head_result.text)
 
+    #print("Last block: ", last_block)
+    counter = 0
     for scan in scans_folders:
-        for root, dirs, files in os.walk(path + '/' + scan):
+        for root, dirs, files in os.walk(path + '\\' + scan):
             images_json = []
             for name in files:
                 if name.endswith((".hash")):
                     folder = os.listdir(root)
                     if name.split('.')[0] + '.sign' in folder:
-                        images_json.append(get_images_json(root + '/' + name, name.split('.')[0], root.split('/')[-3], root.split('/')[-2], root.split('/')[-1]))
-            print('data', json.dumps(images_json, indent=4))
-            last_block = send_block(json.dumps(images_json, indent=4), last_block)
+                        counter += 1
+                        images_json.append(get_images_json(root + '/' + name, name.split('.')[0], root.split('\\')[-3], root.split('\\')[-2], root.split('\\')[-1]))
+                        if counter == 50:
+                            print('data', json.dumps(images_json, indent=4))
+                            last_block = send_block(json.dumps(images_json, indent=4), last_block, counter)
+                            images_json = []
+                            counter = 0
+            if counter > 0:
+                print('data', json.dumps(images_json, indent=4))
+                last_block = send_block(json.dumps(images_json, indent=4), last_block, counter)
+                images_json = []
+                counter = 0
 
 search_files(dest_folder)
